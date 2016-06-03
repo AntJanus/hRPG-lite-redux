759b6d263e10ee38c23fc37df2fb2e41077f4f74
Antonin Januska <antonin@antjanus.com>
2016-06-03 15:41:06 -0600

    Check task on complete, disable task when unavailable

--- src/app/views/task.js
+++ src/app/views/task.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
 
+const date = new Date();
+
+const day = date.getDay();
+
+const days = [
+  'su',
+  'm',
+  't',
+  'w',
+  'th',
+  'f',
+  's'
+];
+
 class Task extends Component {
 
   constructor(props) {
     super(props);
   }
 
+  isAvailable(taskRepeat) {
+      var currentDay = days[day];
+
+    return !taskRepeat || taskRepeat[currentDay];
+  }
+
   render() {
     const { onCompleteTask, task } = this.props;
 
-    let taskState = task.completed ? '' : parseInt(task.value) > 0 ? 'task-positive' : parseInt(task.value) < 0 ? 'task-negative' : 'task-neutral';
+    let taskState = task.completed || !this.isAvailable(task.repeat) ? '' : parseInt(task.value) > 0 ? 'task-positive' : parseInt(task.value) < 0 ? 'task-negative' : 'task-neutral';
+    let taskIcon = task.completed ? 'fa fa-check-square-o' : 'fa fa-square-o';
 
     return (
       <li className={taskState}>
           <a className="task-action"
             onClick={() => onCompleteTask(task)}>
-            <span className="fa fa-square-o"></span>
+            <span className={taskIcon}></span>
           </a>
 
           <span className="task-content">
