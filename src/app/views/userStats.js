25375a502739d1438ce13691fb8426de16c684b9
Antonin Januska <antonin@antjanus.com>
2016-06-28 23:46:12 -0600

    Bar container react tstyle

--- src/app/views/userStats.js
+++ src/app/views/userStats.js
@@ -11,12 +11,40 @@ class UserStats extends Component {
 
     const stats = user.stats || {};
 
+    var hpStyle = {
+      backgroundColor: 'red',
+      width: (stats.hp / 50 * 100)+ '%'
+    };
+
+    var mpStyle = {
+      backgroundColor: 'blue',
+      width: stats.mp + '%'
+    };
+
+    var lvlStyle = {
+      backgroundColor: 'yellow',
+      width: stats.lvl + '%'
+    };
+
     return (
       <div>
         User stats:
-            <div><i className="fa fa-heart"></i> {stats.hp}</div>
-            <div><i className="fa fa-fire"></i> {stats.mp}</div>
-            <div><i className="fa fa-star"></i> {stats.lvl}</div>
+            <div>
+              <i className="fa fa-heart"></i>
+              <div class="bar-container">
+                <div class="bar-graph" style={hpStyle}>{stats.hp}</div>
+              </div>
+            </div>
+            <div><i className="fa fa-fire"></i>
+              <div class="bar-container">
+                <div class="bar-graph" style={mpStyle}>{stats.mp}</div>
+              </div>
+            </div>
+            <div><i className="fa fa-star"></i>
+              <div class="bar-container">
+                <div class="bar-graph" style={lvlStyle}>{stats.lvl}</div>
+              </div>
+            </div>
       </div>
     );
   }
